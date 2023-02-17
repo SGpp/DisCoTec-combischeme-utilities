bd41ef7ae1317b030fd0aba434ca055d8be1681f
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2023-02-17 10:55:20 +0100

    add script: shift levels in combi scheme

---
+++ shift_levels_in_combischeme.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import argparse
+from icecream import ic
+import combischeme_utils
+import combischeme_output
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "file_name",
+        type=str,
+        default="scheme.json",
+    )
+    parser.add_argument(
+        "--level",
+        nargs="*",
+        type=int
+    )
+
+    args = parser.parse_args()
+    level = args.level
+    filename = args.file_name
+    ic(filename, level)
+
+    scheme = combischeme_utils.CombinationSchemeFromFile(
+        filename, boundary_points=1)
+
+    dim = scheme.get_dimensionality()
+    assert (dim == len(level))
+    lmaxBefore = scheme.get_lmax()
+    ic(dim, lmaxBefore)
+    totalNumPointsCombiBefore = scheme.get_total_num_points_combi()
+
+    ic(scheme.get_num_grids_per_level_sum())
+    ic(totalNumPointsCombiBefore, totalNumPointsCombiBefore/1e13,
+       combischeme_output.readable_bytes(totalNumPointsCombiBefore*8))
+
+    # change every key in combination dictionary by level
+    combidictBefore = scheme.get_combination_dictionary()
+    ic(combidictBefore)
+    combidictAfter = {}
+    for key in combidictBefore:
+        combidictAfter[tuple([l + level[i]
+                             for i, l in enumerate(key)])] = combidictBefore[key]
+
+    ic(combidictAfter)
+    schemeAfter = combischeme_utils.CombinationSchemeFromCombinationDictionary(
+        combidictAfter, boundary_points=scheme.get_boundary_points())
+    assert (schemeAfter.get_total_num_points_combi() ==
+            totalNumPointsCombiBefore * 2**(sum(level)))
+    lmaxAfter = schemeAfter.get_lmax()
+    ic(lmaxAfter)
+    assert (all(lmaxAfter == [l + level[i] for i, l in enumerate(lmaxBefore)]))
+
+    combischeme_utils.write_scheme_to_json(
+        schemeAfter, "scheme_large_" + '-'.join([str(l) for l in lmaxAfter]) + ".json")
