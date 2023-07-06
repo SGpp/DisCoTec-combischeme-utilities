0e8c18e1ccfb4aaf466f3e22c8f665d05511ba55
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2023-07-06 15:27:08 +0200

    partitions_to_combischeme: respect existing groups

--- partitions_to_combischeme.py
+++ partitions_to_combischeme.py
@@ -23,15 +23,41 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    first_scheme = combischeme_utils.CombinationSchemeFromFile(args.partitioned_files[0])
+    first_scheme = combischeme_utils.CombinationSchemeFromFile(
+        args.partitioned_files[0])
 
-    partitions = [combischeme_utils.CombinationSchemeFromFile(file) for file in args.partitioned_files]
+    # divide json-dictionary into assigned partitions
+    partition_dicts = []
 
-    if args.out_name is None:
-        args.out_name = "combined_"+args.partitioned_files[0]
+    for file in args.partitioned_files:
+        data = combischeme_output.read_data_from_json(file)
+        # collect all different process group numbers
+        group_numbers = set()
+        try:
+            for grid in data:
+                group_numbers.add(grid['group_no'])
+
+            if len(group_numbers) > 1:
+                for group_no in group_numbers:
+                    combination_dictionary = {}
+                    for grid in data:
+                        if grid['group_no'] == group_no:
+                            combination_dictionary[tuple(
+                                grid['level'])] = grid['coeff']
+                    partition_dicts.append(combination_dictionary)
+        except KeyError:
+            combination_dictionary = {}
+            for grid in data:
+                combination_dictionary[tuple(grid['level'])] = grid['coeff']
+            partition_dicts.append(combination_dictionary)
+
+    partitions = [combischeme_utils.CombinationSchemeFromCombinationDictionary(
+        dict) for dict in partition_dicts]
 
     assignment = [p.get_combination_dictionary() for p in partitions]
 
+    if args.out_name is None:
+        args.out_name = "combined_"+args.partitioned_files[0]
     combischeme_output.write_assignment_to_json(assignment, args.out_name)
 
     # for testing, with checks?
